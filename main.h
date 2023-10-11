c93350999663b59153d509979937fcb02ff0175a
nada <nazaeissa@gmail.com>
2023-10-11 20:48:11 +0200

    shell

--- main.h
+++ main.h
@@ -1,5 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <fcnt1.h>
+
+
+
+
 
 
 #endif
